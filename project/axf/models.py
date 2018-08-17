917f403ad5d23a77989f70388df8c54dbed85176
zcs <zcs@163.com>
2018-08-17 10:41:48 +0800

    2017-8-17 10:41

--- project/axf/models.py
+++ project/axf/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Wheel(models.Model):
+    img = models.CharField(max_length=150)
+    name = models.CharField(max_length=50)
+    trackid = models.CharField(max_length=20)
+    isDetele = models.BooleanField(default=False)
--- project/axf/views.py
+++ project/axf/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
-#主页
+# 主页
 def home(request):
-    retuen render(request,'axf/home.html')
+    title = '主页'
+    return render(request, 'axf/home.html',{'title':title})
 
-#闪送超市
+
+# 闪送超市
 def market(request):
-    retuen render(request,'axf/market.html')
-    
-#购物车
+    title = '闪送超市'
+    return render(request, 'axf/market.html',{'title':title})
+
+
+# 购物车
 def cart(request):
-    retuen render(request,'axf/cart.html')
-    
-#我的页面
+    title = '购物车'
+    return render(request, 'axf/cart.html',{'title':title})
+
+
+# 我的页面
 def mine(request):
-    retuen render(request,'axf/mine.html')
+    title = '我的页面'
+    return render(request, 'axf/mine.html',{'title':title})
